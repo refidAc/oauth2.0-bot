458a07833838aa881003e0dd3ed2f059b2b1f4c8
RefidAc <xmists2@protonmail.com>
2024-01-21 01:09:33 -0500

    1

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def refresh_token():
     st_refreshed_token = '"{}"'.format(refreshed_token)
     j_refreshed_token = json.loads(st_refreshed_token)
     r.set("token", j_refreshed_token)
-    return json.dumps({"Token Refreshed?":j_refreshed_token})
+    return json.dumps({"PreviousToken":t,"Token Refreshed?":j_refreshed_token})
 
 
 @app.route("/")
