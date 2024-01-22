8b18350259889b5d262d7ab1a1ca25393d804f92
RefidAc <xmists2@protonmail.com>
2024-01-22 00:30:33 -0500

    1

--- main.py
+++ main.py
@@ -161,15 +161,16 @@ def retweet():
 
 def post_tweet(payload, token):
     print("Tweeting!")
-    return requests.request(
-        "POST",
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-        headers={
-            "Authorization": "Bearer {}".format(token["access_token"]),
-            "Content-Type": "application/json",
-        },
-    )
+    # return requests.request(
+    #     "POST",
+    #     "https://api.twitter.com/2/tweets",
+    #     json=payload,
+    #     headers={
+    #         "Authorization": "Bearer {}".format(token["access_token"]),
+    #         "Content-Type": "application/json",
+    #     },
+    # )
+    return json.dumps({"pretend like i tweeted":"something"})
 
 @app.route("/testrefresh", methods=["GET"])
 def refresh_token():
